6aa8e287109a21ad4493327ff47083fa4eba9f4f
HeeSeok Lee <dlgmltjr0925@gmail.com>
2025-06-17 00:59:55 +0900

    feature: Button 컴포넌트 구현

--- src/app/(dev)/design-system/button/page.tsx
+++ src/app/(dev)/design-system/button/page.tsx
@@ -1,16 +1,68 @@
 import { Button } from "~/client/shared/ui/button";
 
+const variants = ["text", "contained", "outlined"] as const;
+const colors = [
+  "primary",
+  "secondary",
+  "error",
+  "warning",
+  "success",
+  "info",
+] as const;
+
+const sizes = ["small", "medium", "large"] as const;
+
 export default function ButtonPage() {
   return (
-    <div className="p-4">
+    <div className="flex flex-col gap-4 p-4">
       <h1 className="text-overline">Button</h1>
 
-      <h2 className="text-subtitle1">Variant</h2>
+      <div>
+        <h2 className="text-subtitle1">Variant</h2>
+        <div className="flex flex-row gap-2">
+          {variants.map((variant) => (
+            <Button key={variant} variant={variant}>
+              {variant}
+            </Button>
+          ))}
+        </div>
+      </div>
+
+      <div>
+        <h2>Disabled</h2>
+        <div className="flex flex-row gap-2">
+          {variants.map((variant) => (
+            <Button key={variant} variant={variant} disabled>
+              {variant}
+            </Button>
+          ))}
+        </div>
+      </div>
 
       <div className="flex flex-col gap-2">
-        <Button variant="text">Text</Button>
-        <Button variant="contained">Contained</Button>
-        <Button variant="outlined">Outlined</Button>
+        <h2 className="text-subtitle1">Color</h2>
+        {colors.map((color) => (
+          <div key={color} className="flex flex-row gap-2">
+            {variants.map((variant) => (
+              <Button key={variant} variant={variant} color={color}>
+                {color}
+              </Button>
+            ))}
+          </div>
+        ))}
+      </div>
+
+      <div className="flex flex-col gap-2">
+        <h2 className="text-subtitle1">Size</h2>
+        {variants.map((variant) => (
+          <div key={variant} className="flex flex-row items-center gap-2">
+            {sizes.map((size) => (
+              <Button key={size} variant={variant} size={size}>
+                {size}
+              </Button>
+            ))}
+          </div>
+        ))}
       </div>
     </div>
   );
--- src/client/shared/ui/button/Button.tsx
+++ src/client/shared/ui/button/Button.tsx
@@ -1,28 +1,70 @@
-import "./button.css";
 import { ComponentProps } from "react";
 import { twMerge } from "tailwind-merge";
 
 export interface ButtonProps extends ComponentProps<"button"> {
   variant?: "text" | "contained" | "outlined";
   disabled?: boolean;
-  color?: "primary" | "secondary" | "error";
+  color?: "primary" | "secondary" | "error" | "warning" | "success" | "info";
   size?: "small" | "medium" | "large";
+  startIcon?: React.ReactNode;
+  endIcon?: React.ReactNode;
 }
 
-const buttonVariants: Record<NonNullable<ButtonProps["variant"]>, string> = {
-  text: "button-variant-text",
-  contained: "button-variant-contained",
-  outlined: "button-variant-outlined",
+const buttonVariants: Record<string, string> = {
+  text: "button-text",
+  contained: "button-contained",
+  outlined: "button-outlined",
+};
+
+const buttonColors: Record<string, string> = {
+  "text-primary": "button-text-primary",
+  "text-secondary": "button-text-secondary",
+  "text-error": "button-text-error",
+  "text-warning": "button-text-warning",
+  "text-success": "button-text-success",
+  "text-info": "button-text-info",
+  "contained-primary": "button-contained-primary",
+  "contained-secondary": "button-contained-secondary",
+  "contained-error": "button-contained-error",
+  "contained-warning": "button-contained-warning",
+  "contained-success": "button-contained-success",
+  "contained-info": "button-contained-info",
+  "outlined-primary": "button-outlined-primary",
+  "outlined-secondary": "button-outlined-secondary",
+  "outlined-error": "button-outlined-error",
+  "outlined-warning": "button-outlined-warning",
+  "outlined-success": "button-outlined-success",
+  "outlined-info": "button-outlined-info",
+};
+
+const buttonSize: Record<string, string> = {
+  small: "button-size-small",
+  medium: "button-size-medium",
+  large: "button-size-large",
 };
 
 export const Button = ({
   children,
   variant = "text",
+  color = "primary",
+  size = "medium",
+  startIcon,
+  endIcon,
   ...props
 }: ButtonProps) => {
   return (
-    <button className={twMerge("button", buttonVariants[variant])} {...props}>
+    <button
+      className={twMerge(
+        "button",
+        buttonVariants[variant],
+        buttonColors[`${variant}-${color}`],
+        buttonSize[size],
+      )}
+      {...props}
+    >
+      {startIcon && <span className="mr-2">{startIcon}</span>}
       {children}
+      {endIcon && <span className="ml-2">{endIcon}</span>}
     </button>
   );
 };
