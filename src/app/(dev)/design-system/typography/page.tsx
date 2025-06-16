7d5d9307dbbba1d4bfb73e66d9781bcea4fce105
HeeSeok Lee <dlgmltjr0925@gmail.com>
2025-06-16 20:58:46 +0900

    feature: typography class name utility 변경

--- src/app/(dev)/design-system/typography/page.tsx
+++ src/app/(dev)/design-system/typography/page.tsx
@@ -4,17 +4,17 @@ export default function TypographyPage() {
       <h1 className="text-overline">Typography</h1>
 
       <div>
-        <h1 className="text-h1">Heading 1</h1>
-        <h2 className="text-h2">Heading 2</h2>
-        <h3 className="text-h3">Heading 3</h3>
-        <h4 className="text-h4">Heading 4</h4>
-        <h5 className="text-h5">Heading 5</h5>
-        <h6 className="text-h6">Heading 6</h6>
-        <p className="text-body1">Body 1</p>
-        <p className="text-body2">Body 2</p>
-        <p className="text-button">Button</p>
-        <caption className="text-caption">Caption</caption>
-        <p className="text-overline">Overline</p>
+        <h1 className="typo-h1">Heading 1</h1>
+        <h2 className="typo-h2">Heading 2</h2>
+        <h3 className="typo-h3">Heading 3</h3>
+        <h4 className="typo-h4">Heading 4</h4>
+        <h5 className="typo-h5">Heading 5</h5>
+        <h6 className="typo-h6">Heading 6</h6>
+        <p className="typo-body1">Body 1</p>
+        <p className="typo-body2">Body 2</p>
+        <p className="typo-button">Button</p>
+        <caption className="typo-caption">Caption</caption>
+        <p className="typo-overline">Overline</p>
       </div>
     </div>
   );
