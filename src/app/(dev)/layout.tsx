2c330659ed5272e23b1409e0c942929751037823
HeeSeok Lee <dlgmltjr0925@gmail.com>
2025-06-16 16:57:38 +0900

    feature: design system page 생성 및 운영 환경에서 redirect 처리

---
+++ src/app/(dev)/layout.tsx
@@ -0,0 +1,13 @@
+import { redirect } from 'next/navigation';
+
+export default function DevLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  if (process.env.NODE_ENV === 'production') {
+    redirect('/');
+  }
+
+  return children;
+}
