fc7a8259727fc8e6bb1c19eec8d706ad2ba9ed17
HeeSeok Lee <dlgmltjr0925@gmail.com>
2025-06-16 19:31:44 +0900

    feature: typography 토큰 추가

---
+++ src/app/(dev)/design-system/typography/page.tsx
@@ -0,0 +1,21 @@
+export default function TypographyPage() {
+  return (
+    <div>
+      <h1 className="text-overline">Typography</h1>
+
+      <div>
+        <h1 className="text-h1">Heading 1</h1>
+        <h2 className="text-h2">Heading 2</h2>
+        <h3 className="text-h3">Heading 3</h3>
+        <h4 className="text-h4">Heading 4</h4>
+        <h5 className="text-h5">Heading 5</h5>
+        <h6 className="text-h6">Heading 6</h6>
+        <p className="text-body1">Body 1</p>
+        <p className="text-body2">Body 2</p>
+        <p className="text-button">Button</p>
+        <caption className="text-caption">Caption</caption>
+        <p className="text-overline">Overline</p>
+      </div>
+    </div>
+  );
+}
